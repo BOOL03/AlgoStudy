26835a789987a20afed94286a75414498d2710ba
leedayeong <98dlstod@naver.com>
2022-03-26 23:51:40 +0900

    이다영

---
+++ src/study_0325/BJ_20920_이다영.java
@@ -0,0 +1,44 @@
+package day_03_25;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class BJ_알파벳사전순_20920_1 {
+	static Map<String, Integer> hmMap = new HashMap<>();
+	static int N, M;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			if(str.length() < M) continue;
+			if(hmMap.containsKey(str)) {
+				hmMap.put(str, hmMap.get(str) + 1);
+			}else hmMap.put(str, 1);
+		}
+		
+		List<String> listKeySet = new ArrayList<>(hmMap.keySet());
+		
+		Collections.sort(listKeySet, (v1, v2) -> ((hmMap.get(v2) == (hmMap.get(v1))? (v2.length() ==  v1.length()? v1.compareTo(v2) : v2.length() - v1.length()) : hmMap.get(v2) - hmMap.get(v1))));
+		
+		for (String key : listKeySet) {
+			sb.append(key).append("\n");
+		}
+		
+		System.out.println(sb);
+	
+//		Collections.sort(listKeySet, (k1, k2) -> k2.length() ==  k1.length()? k1.compareTo(k2) : k2.length() - k1.length());
+
+	}
+}
---
+++ src/study_0325/BJ_2531_이다영.java
@@ -0,0 +1,45 @@
+package day_03_25;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class BJ_회전초밥_2531 {
+	static int N, d, k, c, ans;
+	static List<Integer> dish;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		d = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		
+		dish = new ArrayList<Integer>();
+		
+		for (int i = 0; i < N; i++) {
+			dish.add(Integer.parseInt(br.readLine()));
+		}
+		
+		for (int i = N; i < N+k-1; i++) {
+			dish.add(dish.get(i%N));
+		}
+		
+		ans = Integer.MIN_VALUE;
+		for (int i = 0; i < N; i++) {
+			Set<Integer> kindOfDish = new HashSet<>( dish.subList(i, i+k) );
+			int size = kindOfDish.contains(c) ? kindOfDish.size() : kindOfDish.size()+1;
+			if(size == d) {
+				System.out.println(d);
+				return;
+			}
+			ans = Math.max(ans, size);
+		}
+		System.out.println(ans);
+	}
+}
