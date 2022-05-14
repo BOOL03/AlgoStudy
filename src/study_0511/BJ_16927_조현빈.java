57e651d176ce46adf0f7987182c20e3edf605542
JHB <jo7hb@naver.com>
2022-05-14 13:35:41 +0900

    조현빈

--- src/study_0511/BJ_16927_조현빈.java
+++ src/study_0511/BJ_16927_조현빈.java
@@ -1,28 +1,76 @@
 package study_0511;
 
-import java.io.*;
 import java.util.*;
 
 public class BJ_16927_조현빈 {
 
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
-    int R = Integer.parseInt(st.nextToken());
-    int[][] arr = new int[N][M];
-    int count = N * M;
-    boolean[][] visit = new boolean[N][M];
-    int[] dx = { 0, 1, 0, -1 };
-    int[] dy = { -1, 0, 1, 0 };
+  static int N;
+  static int M;
+  static int rotate;
+  static int[][] array;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    M = sc.nextInt();
+    rotate = sc.nextInt();
+    array = new int[N][M];
 
     for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
       for (int j = 0; j < M; j++) {
-        arr[i][j] = Integer.parseInt(st.nextToken());
+        array[i][j] = sc.nextInt();
       }
+    }
+
+    int rowStart = 0;
+    int rowEnd = N - 1;
+
+    int colStart = 0;
+    int colEnd = M - 1;
+    while (true) {
+      int size = (rowEnd - rowStart + 1) * 2 + (colEnd - colStart + 1) * 2 - 4;
+      change(rowStart, rowEnd, colStart, colEnd, rotate % size);
+      rowStart += 1;
+      rowEnd -= 1;
+      colStart += 1;
+      colEnd -= 1;
+      if (rowStart > rowEnd || colStart > colEnd) break;
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        System.out.print(array[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void change(
+    int rowStart,
+    int rowEnd,
+    int colStart,
+    int colEnd,
+    int cnt
+  ) {
+    for (int k = 0; k < cnt; k++) {
+      int temp = array[rowStart][colStart];
+
+      for (int j = colStart; j < colEnd; j++) {
+        array[rowStart][j] = array[rowStart][j + 1];
+      }
+
+      for (int i = rowStart; i < rowEnd; i++) {
+        array[i][colEnd] = array[i + 1][colEnd];
+      }
+
+      for (int j = colEnd; j > colStart; j--) {
+        array[rowEnd][j] = array[rowEnd][j - 1];
+      }
+
+      for (int i = rowEnd; i > rowStart; i--) {
+        array[i][colStart] = array[i - 1][colStart];
+      }
+      array[rowStart + 1][colStart] = temp;
     }
   }
 }
